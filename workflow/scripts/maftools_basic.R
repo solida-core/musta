f6b08c2db0e32ac808f2af33f183336d6f33e6d1
ratzeni <rossano.atzeni@crs4.it>
2024-05-08 12:02:28 +0200

    maftools - base (try..catch)

--- workflow/scripts/maftools_basic.R
+++ workflow/scripts/maftools_basic.R
@@ -8,10 +8,10 @@ library('stringr')
 
 sessionInfo()
 ## input
-input_maf=snakemake@input[["mafs"]]
+input_maf <- snakemake@input[["mafs"]]
 print(input_maf)
-project_id=snakemake@params[["project_id"]]
-output_path=snakemake@params[["outdir"]]
+project_id <- snakemake@params[["project_id"]]
+output_path <- snakemake@params[["outdir"]]
 ## combine input files
 getwd()
 
@@ -28,28 +28,28 @@ dir.exists(output_path)
 message("Output path is: ",output_path)
 
 ## check from maf
-aminoacid_cname='Protein_Change'
+aminoacid_cname <- 'Protein_Change'
 if('HGVSp' %in% names(my_maf@data)){
-  aminoacid_cname='HGVSp'
+  aminoacid_cname <- 'HGVSp'
 }
 aminoacid_cname
-genome_vers='hg19'
-genome_lib="BSgenome.Hsapiens.UCSC.hg19"
-genome_strings=c("hg38","GRCh38")
+genome_vers <- 'hg19'
+genome_lib <- "BSgenome.Hsapiens.UCSC.hg19"
+genome_strings <- c("hg38", "GRCh38")
 if(my_maf@data[1,"NCBI_Build"] %in% genome_strings){
-  genome_vers='hg38'
-  genome_lib="BSgenome.Hsapiens.UCSC.hg38"
+  genome_vers <- 'hg38'
+  genome_lib <- "BSgenome.Hsapiens.UCSC.hg38"
   library("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE)
 }
 genome_vers
 genome_lib
 
-chr_prefix=NULL
-add_prefix=F
+chr_prefix <- NULL
+add_prefix <- F
 # default considera che il maf abbia i chr nominati chr1, chr2 ecc
 if(str_detect(my_maf@data[1,"Chromosome"], pattern = "chr", negate = T)){
-  chr_prefix='chr'
-  add_prefix=T
+  chr_prefix <- 'chr'
+  add_prefix <- T
 }
 chr_prefix
 add_prefix
@@ -79,8 +79,8 @@ dev.off()
 
 
 #oncoplot for top ten mutated genes.
-vc_cols = RColorBrewer::brewer.pal(n = 8, name = 'Paired')
-names(vc_cols) = c(
+vc_cols <- RColorBrewer::brewer.pal(n = 8, name = 'Paired')
+names(vc_cols) <- c(
   'Frame_Shift_Del',
   'Missense_Mutation',
   'Nonsense_Mutation',
@@ -104,10 +104,10 @@ oncoplot(maf = my_maf, colors = vc_cols, altered = F,
          drawBox = F, titleText = "Oncoplot 50 genes", legendFontSize = 1.6, drawColBar = F, top = 50, sortByMutation = F)
 dev.off()
 
-actual_path=getwd()
-z=read.table(file.path(actual_path,"onco_matrix.txt"), sep = "\t")
+actual_path <- getwd()
+z <- read.table(file.path(actual_path, "onco_matrix.txt"), sep = "\t")
 
-my_maf.titv = titv(maf = my_maf, plot = FALSE, useSyn = TRUE)
+my_maf.titv <- titv(maf = my_maf, plot = FALSE, useSyn = TRUE)
 titv(maf = my_maf, plot = FALSE, useSyn = TRUE, file = file.path(output_path,"tables","transitions_and_transversions"))
 #plot titv summary
 png(filename = file.path(output_path,"plots","titv.png"), width = 500, height = 250, units='mm', res = 400)
@@ -132,15 +132,15 @@ for(gene in rownames(z)){
 
 }
 
-samples=as.list(as.data.frame(getSampleSummary(my_maf))["Tumor_Sample_Barcode"])
-samples=as.list(samples$Tumor_Sample_Barcode)
+samples <- as.list(as.data.frame(getSampleSummary(my_maf))["Tumor_Sample_Barcode"])
+samples <- as.list(samples$Tumor_Sample_Barcode)
 
 dir.create(file.path(output_path,"plots","rainfall"), showWarnings = F)
 dir.exists(file.path(output_path,"plots","rainfall"))
-i=0
+i <- 0
 for(sample in samples){
   print(sample)
-  i=i+1
+  i <- i+1
   print(paste("samples processed:",i,"/",length(samples)))
   try(expr = {png(filename = file.path(output_path,"plots","rainfall",paste(sample,".rainfall.png")), width = 500, height = 250, units='mm', res = 400)
     rainfallPlot(maf = my_maf, detectChangePoints = TRUE, pointSize = 0.4, ref.build = genome_vers, tsb=sample)
@@ -150,7 +150,7 @@ for(sample in samples){
 
 
 png(filename = file.path(output_path,"plots","TGCA_compare.png"), width = 500, height = 250, units='mm', res = 400)
-my_maf.mutload = tcgaCompare(maf = my_maf, cohortName = project_id, logscale = TRUE, capture_size = 50)
+my_maf.mutload <- tcgaCompare(maf = my_maf, cohortName = project_id, logscale = TRUE, capture_size = 50)
 dev.off()
 
 write.table(my_maf.mutload$median_mutation_burden, file = file.path(output_path,"tables","TGCA_median_mutation_burden.tsv"), sep = "\t", row.names = F, col.names = T)
@@ -159,11 +159,20 @@ write.table(my_maf.mutload$mutation_burden_perSample, file = file.path(output_pa
 
 write.table(my_maf.mutload$pairwise_t_test, file = file.path(output_path,"tables","TGCA_pairwise_t_test.tsv"), sep = "\t", row.names = F, col.names = T)
 
+tryCatch({
+    png(filename = file.path(output_path,"plots","top10_VAF.png"), width = 500, height = 250, units='mm', res = 400)
+    plotVaf(maf = my_maf, top = 10, showN = T)
+    dev.off()
+}, error = function(err) {
+  print("Error while plotting VAF (top genes: 10)")
+  print(err)
+})
 
-png(filename = file.path(output_path,"plots","top10_VAF.png"), width = 500, height = 250, units='mm', res = 400)
-plotVaf(maf = my_maf, top = 10, showN = T)
-dev.off()
-
-png(filename = file.path(output_path,"plots","top20_VAF.png"), width = 500, height = 250, units='mm', res = 400)
-plotVaf(maf = my_maf, top = 20, showN = T)
-dev.off()
+tryCatch({
+    png(filename = file.path(output_path,"plots","top20_VAF.png"), width = 500, height = 250, units='mm', res = 400)
+    plotVaf(maf = my_maf, top = 20, showN = T)
+    dev.off()
+}, error = function(err) {
+  print("Error while plotting VAF (top genes: 20)")
+  print(err)
+})
