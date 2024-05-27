320fcda63a3e23715a3c41e897a5df7c82c4a47d
ratzeni <rossano.atzeni@crs4.it>
2024-05-27 18:46:22 +0200

    maftools - pathways

--- workflow/scripts/maftools_pathway.R
+++ workflow/scripts/maftools_pathway.R
@@ -8,17 +8,18 @@ library('stringr')
 
 sessionInfo()
 ## input
-input_maf=snakemake@input[["mafs"]]
-print(input_maf)
-project_id=snakemake@params[["project_id"]]
-output_path=snakemake@params[["outdir"]]
+input_maf <- snakemake@input[["mafs"]]
+message("Input files:", input_maf)
+
+project_id <- snakemake@params[["project_id"]]
+output_path <- snakemake@params[["outdir"]]
 all_variants <- as.logical(snakemake@params[["all_variants"]])
 ## combine input files
 getwd()
 
 maf.filenames <- file.path(input_maf)
-list.all.maf.files <- lapply(maf.filenames,function(i){
-  read.delim(i, sep = "\t", header = TRUE, fill = TRUE, comment.char = "#")
+list.all.maf.files <- lapply(maf.filenames, function(i) {
+    read.delim(i, sep = "\t", header = TRUE, fill = TRUE, comment.char = "#")
 })
 
 ###merging the all the .maf files
@@ -39,51 +40,55 @@ if (all_variants == TRUE) {
 ## create out dir
 dir.create(file.path(output_path), showWarnings = F)
 dir.exists(output_path)
-message("Output path is: ",output_path)
+message("Output path is: ", output_path)
 
 ## check from maf
-aminoacid_cname='Protein_Change'
-if('HGVSp' %in% names(my_maf@data)){
-  aminoacid_cname='HGVSp'
+aminoacid_cname <- 'Protein_Change'
+if ('HGVSp' %in% names(my_maf@data)) {
+    aminoacid_cname <- 'HGVSp'
 }
-aminoacid_cname
-genome_vers='hg19'
-genome_lib="BSgenome.Hsapiens.UCSC.hg19"
-genome_strings=c("hg38","GRCh38")
-if(my_maf@data[1,"NCBI_Build"] %in% genome_strings){
-  genome_vers='hg38'
-  genome_lib="BSgenome.Hsapiens.UCSC.hg38"
-  library("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE)
+# aminoacid_cname
+genome_vers <- 'hg19'
+genome_lib <- "BSgenome.Hsapiens.UCSC.hg19"
+genome_strings <- c("hg38", "GRCh38")
+if (my_maf@data[1, "NCBI_Build"] %in% genome_strings) {
+    genome_vers <- 'hg38'
+    genome_lib <- "BSgenome.Hsapiens.UCSC.hg38"
+    library("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE)
 }
-genome_vers
-genome_lib
 
-chr_prefix=NULL
-add_prefix=F
+message("Genome Version: ", genome_vers)
+message("Genome Library: ", genome_lib)
+
+chr_prefix <- NULL
+add_prefix <- F
 # default considera che il maf abbia i chr nominati chr1, chr2 ecc
-if(str_detect(my_maf@data[1,"Chromosome"], pattern = "chr", negate = T)){
-  chr_prefix='chr'
-  add_prefix=T
+if (str_detect(my_maf@data[1, "Chromosome"], pattern = "chr", negate = T)) {
+    chr_prefix <- 'chr'
+    add_prefix <- T
 }
-chr_prefix
-add_prefix
+# chr_prefix
+# add_prefix
 
 ## summaty tables
-message("Processing MAF for General Statistics")
-dir.create(file.path(output_path,"tables"), showWarnings = F)
-dir.create(file.path(output_path,"plots"), showWarnings = F)
+message("Processing MAF for Oncogenic Pathways")
+dir.create(file.path(output_path, "tables"), showWarnings = F)
+dir.create(file.path(output_path, "plots"), showWarnings = F)
 
-png(filename = file.path(output_path,"plots","oncogenic_pathways.png"), width = 500, height = 250, units='mm', res = 400)
+png(filename = file.path(output_path, "plots", "oncogenic_pathways.png"), width = 500, height = 250, units = 'mm', res = 400)
 OncogenicPathways(maf = my_maf, fontSize = 1.5)
 dev.off()
 
-pathways=as.data.frame(OncogenicPathways(maf = my_maf))
-write.table(pathways, file = file.path(output_path,"tables","oncogenic_pathways.tsv"), sep = "\t", row.names = F, col.names = T)
+pathways <- as.data.frame(OncogenicPathways(maf = my_maf))
+write.table(pathways, file = file.path(output_path, "tables", "oncogenic_pathways.tsv"), sep = "\t", row.names = F, col.names = T)
 
-dir.create(file.path(output_path,"plots","pathways"), showWarnings = F)
-dir.exists(file.path(output_path,"plots","pathways"))
-for(pathway in pathways$Pathway){
-  png(filename = file.path(output_path,"plots","pathways",paste(pathway,"_pathway.png",sep = "")), width = 500, height = 250, units='mm', res = 400)
-  PlotOncogenicPathways(maf = my_maf, pathways = pathway, showTumorSampleBarcodes = F, SampleNamefontSize = 0.4)
-  dev.off()
+dir.create(file.path(output_path, "plots", "pathways"), showWarnings = F)
+dir.exists(file.path(output_path, "plots", "pathways"))
+
+message("Plot oncogenic pathways")
+for (pathway in pathways$Pathway) {
+    print(pathway)
+    png(filename = file.path(output_path, "plots", "pathways", paste(pathway, "_pathway.png", sep = "")), width = 500, height = 250, units = 'mm', res = 400)
+    PlotOncogenicPathways(maf = my_maf, pathways = pathway, showTumorSampleBarcodes = F, SampleNamefontSize = 0.4)
+    dev.off()
 }
